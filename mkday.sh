a1a9474e821fc658dbcf135ebf97ac4c2691bc05
Anders Åstrand <anders@teknodromen.se>
2023-12-07 16:44:30 +0100

    Add user agent

--- mkday.sh
+++ mkday.sh
@@ -8,5 +8,5 @@ cat template.py | sed s/'{{ day }}'/${1}/g > day${1}/day${1}.py
 # Put this in .cookie.txt
 # cookie: session=<token-copied-from-browser-devtools>
 mkdir -p ../aoc-input/$year
-curl -o ../aoc-input/$year/day$1.txt -H @.cookie.txt https://adventofcode.com/$year/day/$1/input
+curl -o ../aoc-input/$year/day$1.txt -H @.cookie.txt -A "mkday.sh by github.com/aastrand via cURL" https://adventofcode.com/$year/day/$1/input
 cat ../aoc-input/$year/day$1.txt 
