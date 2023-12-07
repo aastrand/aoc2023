9e25cc3c07bcdadc59ac29469ae766d336115df1
Anders Åstrand <anders@teknodromen.se>
2023-12-07 16:18:56 +0100

    Show real input in mkday

--- mkday.sh
+++ mkday.sh
@@ -9,3 +9,4 @@ cat template.py | sed s/'{{ day }}'/${1}/g > day${1}/day${1}.py
 # cookie: session=<token-copied-from-browser-devtools>
 mkdir -p ../aoc-input/$year
 curl -o ../aoc-input/$year/day$1.txt -H @.cookie.txt https://adventofcode.com/$year/day/$1/input
+cat ../aoc-input/$year/day$1.txt 
