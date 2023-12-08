36387067e1130c0168c39bca1dc513eeff085d3d
Anders Åstrand <anders@teknodromen.se>
2023-12-08 11:32:55 +0100

    Fix executable flag on template

--- mkday.sh
+++ mkday.sh
@@ -3,10 +3,11 @@
 year=2023
 mkdir -p day$1
 cat template.py | sed s/'{{ day }}'/${1}/g > day${1}/day${1}.py
+chmod +x day${1}/day${1}.py
 
 # Download input
 # Put this in .cookie.txt
 # cookie: session=<token-copied-from-browser-devtools>
-mkdir -p ../aoc-input/$year
-curl -o ../aoc-input/$year/day$1.txt -H @.cookie.txt -A "mkday.sh by github.com/aastrand via cURL" https://adventofcode.com/$year/day/$1/input
-cat ../aoc-input/$year/day$1.txt 
+mkdir -p ../input/$year
+curl -o ../input/$year/day$1.txt -H @.cookie.txt -A "mkday.sh by github.com/aastrand via cURL" https://adventofcode.com/$year/day/$1/input
+cat ../input/$year/day$1.txt 
